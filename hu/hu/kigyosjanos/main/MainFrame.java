805b1c7771f73d7f6663def22e4a76ef29570ded
cycool89 <cycool89@gmail.com>
2015-12-22 14:12:08 +0100

    Init

--- hu/hu/kigyosjanos/main/MainFrame.java
+++ hu/hu/kigyosjanos/main/MainFrame.java
@@ -5,6 +5,6 @@ import javax.swing.JFrame;
 public class MainFrame extends JFrame {
 
   public static void main(String[] args) {
-    System.out.println("Hello World from Git!");
+    System.out.println("Hello World from Git!"); 
   }
 }
