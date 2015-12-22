8196c2856759f03a4cc69eb8447fb294ab82702d
cycool89 <cycool89@gmail.com>
2015-12-22 13:57:30 +0100

    Init

---
+++ hu/kigyosjanos/main/MainFrame.java
@@ -0,0 +1,10 @@
+package hu.kigyosjanos.main;
+
+import javax.swing.JFrame;
+
+public class MainFrame extends JFrame {
+
+  public static void main(String[] args) {
+    System.out.println("Hello World from Git!");
+  }
+}
